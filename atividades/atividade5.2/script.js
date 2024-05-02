fa03989f51792121f24ad03f05ae9139199326ce
marceloSaner <162468533+marceloSaner@users.noreply.github.com>
2024-05-02 15:10:59 -0300

    Atividade 5.2 adicionada.

--- atividades/atividade5.2/script.js
+++ atividades/atividade5.2/script.js
@@ -1,16 +1,16 @@
 
-const elementoTitulo = document.createElement("title")
-elementoTitulo.innerText = "Produtos à Venda"
+const elementoTitulo = document.createElement("h1")
 elementoTitulo.id = "titulo"
-const titulo = document.querySelector("head")
+elementoTitulo.innerText = "Produtos à Venda"
+const titulo = document.querySelector("main")
 titulo.appendChild(elementoTitulo)
-console.log(elementoTitulo)
 
 
 const produto = document.createElement("section")
 produto.innerHTML = 
 `
 <h2 class="produto-titulo">ASSASSIN'S CREED MIRAGE LAUNCH EDITION - PS4</h2>
+<img src="./img/assassinsCreed.PNG" alt="Assassin's Creed Mirage Launch Edition" width="300rem">
 <p class="descricao">Prepare-se para mergulhar em uma jornada épica pela história com Assassin's Creed Mirage Launch Edition para PS4.Entre na pele de um habilidoso assassino e explore os exóticos cenários do Oriente Médio enquanto desvenda segredos ancestrais. Uma experiência única de ação e aventura espera por você. Não perca esta oportunidade de fazer parte da lenda.</p>
 <p class="preco">R$ 268,99</p>
 `
